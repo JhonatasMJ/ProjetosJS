6514e9de82e2785e867708aca0a47b8d8fdf146f
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-08-12 21:13:47 -0300

    Calculo de distancias dos slides

--- Slides/js/slide.js
+++ Slides/js/slide.js
@@ -2,19 +2,39 @@ export default class Slide {
     constructor(slide,wrapper) { 
         this.slide =  document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.dist = { //Distancia
+            finalPosition: 0,
+            startX: 0,
+            movement:0,
+        }
+    }
+
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX
+        this.slide.style.transform = `translate3d(${distX}px,0,0)`
+    }
+
+    //Atualiza a posição do Slide
+    updatePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX) * 1.6; //Velocidade do slide
+        return this.dist.finalPosition - this.dist.movement;
     }
 
     onStart(event) {
         event.preventDefault()
+        this.dist.startX = event.clientX //Mostra a posição que o slide se encontra
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
-    onMove() {
-
+    onMove(event) {
+       const finalPosition =  this.updatePosition(event.clientX);
+       this.moveSlide(finalPosition);
     }
 
     onEnd(event) {
         this.wrapper.removeEventListener('mousemove', this.onMove);
+        this.dist.finalPosition = this.dist.movePosition //Assim que tirar o mouse de cima, salva a posição final do slide
     }
 
     addSlideEvents() {
@@ -25,6 +45,7 @@ export default class Slide {
     bindEvents() {
         this.onStart = this.onStart.bind(this);
         this.onEnd = this.onEnd.bind(this);
+        this.onMove = this.onMove.bind(this);
     }
 
     init() {
